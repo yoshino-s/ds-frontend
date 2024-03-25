fac90c4a9cc00a0be8be107b3b72f7cfa63ac400
yoshino-s <cy-cui@outlook.com>
2024-03-25 13:18:34 +0800

    feat: add sentry sourcemap upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { resolve } from "path";
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "sentry",
+    project: "ds-viewer",
+    url: "https://sentry.yoshino-s.xyz/"
+  })],
+
   resolve: {
     alias: {
       "@/": `${resolve(__dirname, "src")}/`,
     },
   },
+
+  build: {
+    sourcemap: true
+  }
 });
