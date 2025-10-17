fc27fe914e8dec14f4581856261e68625933bc42
yoshino-s <cy-cui@outlook.com>
2025-10-17 12:46:21 +0800

    feat: support customize path for s3

--- app/hooks/useContentFix.ts
+++ app/hooks/useContentFix.ts
@@ -3,7 +3,7 @@ import useConfigStore from "@/store/config";
 export function useContentFix(content?: string | null) {
   const s3Url = useConfigStore((store) => store.s3Url);
   return content?.replace(
-    /https?:\/\/(?:minio-hdd)\.yoshino-s\.(?:online|xyz)\//g,
+    /https?:\/\/minio-hdd\.yoshino-s\.(?:online|xyz)\/crawl\//g,
     s3Url,
   );
 }
